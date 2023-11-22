470be1934d2de46f0dedc44ec88dc15c042fc2fb
khamis_dossary <khamis.dossary@gmail.com>
2023-11-23 02:30:59 +0300

    done

--- blog/main/urls.py
+++ blog/main/urls.py
@@ -5,6 +5,7 @@ app_name = "main"
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/details/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/update/<int:pk>/', views.post_update, name='post_update'),
     path('post/create/', views.post_create, name='post_create'),
 ]
