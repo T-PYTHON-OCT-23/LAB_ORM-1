237fa884c94728221797353a17299fb012d33629
lama1-abdullah <lama.alothemen21@gmail.com>
2023-11-22 13:47:43 +0300

    done exception

--- Blog/post/views.py
+++ Blog/post/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from .models import Blog
+from django.utils import timezone
 # Create your views here.
 
 
 def add_blog_view(request: HttpRequest):
     if request.method == "POST":
-        new_blog = Blog(title=request.POST["title"], content=request.POST["content"], is_published=request.POST["is_published"], Published_at=request.POST["published_at"])
+        new_blog = Blog(title=request.POST["title"], content=request.POST["content"], is_published=request.POST["is_published"], Published_at=timezone.now())
         new_blog.save()
         return redirect("post:display_blog_view")
 
@@ -25,6 +26,7 @@ def display_blog_view(request: HttpRequest):
  
 def post_detail_view(request:HttpRequest, post_id):
 
-    post= Blog.objects.get(id=post_id)
+    post=get_object_or_404(Blog,id=post_id )
+
 
     return render(request, "post/post_detail.html", {"post" : post})
