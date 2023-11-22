c005bacac6d66645c4a29e7a37f8461eb5eac3e8
oudy yousef <oudy.usef@gmail.com>
2023-11-22 13:10:31 +0300

    done ROM 2

--- Blog/blogs/urls.py
+++ Blog/blogs/urls.py
@@ -6,6 +6,8 @@ app_name="blogs"
 urlpatterns = [
 
     path("add/" , views.add_blogs_view, name="add_blogs_view"),
-    path("", views.show_blogs_view, name="show_blogs_view")
+    path("", views.show_blogs_view, name="show_blogs_view"),
+    path("detail/<blog_id>/",views.blog_detail_view, name ="blog_detail_view"),
+    path("not_found/",views.not_found_view, name="not_found_view")
     
 ]
--- Blog/blogs/views.py
+++ Blog/blogs/views.py
@@ -18,3 +18,14 @@ def show_blogs_view(request:HttpRequest):
     blogs=Blog.objects.all()
     
     return render(request,"blogs/show_blogs.html", {"blogs" : blogs})
+
+def not_found_view(request:HttpRequest):
+        return render(request,"blogs/not_found.html")
+
+def blog_detail_view(request:HttpRequest, blog_id):
+    
+    try :
+        blog = Blog.objects.get(id=blog_id)
+        return render(request,"blogs/detail.html" , {"blog":blog})
+    except Exception as e:
+        return redirect("blogs:not_found_view")
