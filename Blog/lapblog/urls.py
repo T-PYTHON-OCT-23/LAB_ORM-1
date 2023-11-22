dcb7fe796ee9b48e7e61bc77c4fbba8a192dcc0c
alya <alyamem80@gmail.com>
2023-11-22 21:30:27 +0300

    done

--- Blog/lapblog/urls.py
+++ Blog/lapblog/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("add/", views.add_blog_view, name="add_blog_view"),
     path("", views.blog_home_view, name="blog_home_view"),
     path("detail/<blog_id>/", views.blog_detail_view, name="blog_detail_view"),
-     path("update/<blog_id>/", views.update_movie_view, name="update_blog_view"),
+     path("update/<blog_id>/", views.update_blog_view, name="update_blog_view"),
   ]
