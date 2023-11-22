dbea41866fd0d9df7180fb7baa48f3f87b173f04
Anoud <Anoudi90@icloud.com>
2023-11-23 01:42:15 +0300

    Done

--- Blog/blogApp/models.py
+++ Blog/blogApp/models.py
@@ -6,6 +6,6 @@ class Blog(models.Model):
 
     title = models.CharField(max_length=2048)
     content = models.TextField()
-    is_published : models.BooleanField()
-    published_at : models.DateTimeField()
+    is_published = models.BooleanField()
+    published_at = models.DateTimeField(auto_now_add=True)
 
--- Blog/blogApp/urls.py
+++ Blog/blogApp/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
 
     path("read/blog/" , views.read_blog_view , name="read_blog_view"),
     path("add/post" , views.add_post_view , name="add_post_view"),
+    path("detail/<blog_id>/" , views.detail_blog_view , name="detail_blog_view"),
+    path("update/<blog_id>/" , views.update_view , name="update_view"),
+    path("delete/<blog_id>/" , views.delete_view , name="delete_view"),
 
 ]
