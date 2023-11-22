fbb8878d0c5955b27503b80262614e77591b016f
m7mdhusinn <m7md.husinn@gmail.com>
2023-11-22 13:00:08 +0300

    done

--- LabORAM/info/urls.py
+++ LabORAM/info/urls.py
@@ -5,6 +5,6 @@ app_name = "info"
 
 urlpatterns = [ 
     path("add/", views.add_info_view, name="add_info_view"),
-    path("", views.info_home_view, name="info_home_view")
-
+    path("", views.info_home_view, name="info_home_view"),
+    path("detail/<info_id>/",views.info_details_view,name="info_details_view"),
 ]
--- LabORAM/info/views.py
+++ LabORAM/info/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.http import HttpRequest, HttpResponse
 from .models import Info
 
@@ -22,7 +22,16 @@ def add_info_view(request: HttpRequest):
 
 
 def info_home_view(request: HttpRequest):
-
     info = Info.objects.all()
-    
+    print(info)
     return render(request, "info/info_home.html", {"info" : info})
+
+    
+        
+def info_details_view(request: HttpRequest,info_id):
+
+    
+    info = get_object_or_404(Info,id=info_id)
+    
+    
+    return render(request, "info/info_details.html", {"info" : info})
