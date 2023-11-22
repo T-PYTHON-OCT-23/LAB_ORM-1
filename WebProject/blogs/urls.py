768603d24da1094a9cd761ca1ab869d2985f124e
Sufana_Aljawi <Sufanaaljawi@gmail.com>
2023-11-23 01:42:11 +0300

    Lab_ORM_ Updated

--- WebProject/blogs/urls.py
+++ WebProject/blogs/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path("", views.blogs_home_view, name="blogs_home_view"),
     path("add/", views.add_blog_view, name="add_blog_view"),
     path("detail/<blog_id>/", views.blogs_details_view, name="blogs_details_view"),
-    path("not", views.not_exist, name="not_exist" ),
+    path("not_exsit/", views.not_exist, name="not_exist" ),
+    path("update/<blog_id>/", views.update_blog_view , name="update_blog_view"),
+    path("delete/<blog_id>/", views.delete_blog_view, name="delete_blog_view"),
 ]
 
 
--- WebProject/blogs/views.py
+++ WebProject/blogs/views.py
@@ -9,8 +9,11 @@ def add_blog_view(request: HttpRequest):
 
     if request.method == "POST":
         new_blog = Blog(title=request.POST["title"], content=request.POST["content"], is_published=request.POST["is_published"], published_at=request.POST["published_at"])
-        new_blog.save()
-
+        try:
+            new_blog.save()
+        except Exception as e:
+            return render(request,"blogs/not_exist.html")
+            
         return redirect("blogs:blogs_home_view")
 
     return render(request, "blogs/add.html")
@@ -28,7 +31,35 @@ def blogs_details_view(request:HttpRequest, blog_id):
         blog = Blog.objects.get(id=blog_id)
     except Exception as e:
         return render(request,"blogs/not_exist.html")
+        
     return render(request , "blogs/blogs_details.html", {"blog":blog})
 
 def not_exist(request:HttpRequest):
     return render(request,"blogs/not_exist.html")
+
+
+def update_blog_view(request: HttpRequest, blog_id):
+    try:
+        blog = Blog.objects.get(id=blog_id)
+    except Exception as e:
+        return render(request,"blogs/not_exist.html")
+
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.content = request.POST["content"]
+        blog.is_published = request.POST["is_published"]
+        blog.published_at = request.POST["published_at"]
+        blog.save()
+
+        return redirect('blogs:blogs_details_view', blog_id=blog.id)
+    
+    return render(request, "blogs/update_blog.html", {"blog" : blog})
+
+
+    
+def delete_blog_view(request:HttpRequest,blog_id):
+    blog= Blog.objects.get(id=blog_id)
+    blog.delete()
+
+    return redirect("blogs:blogs_home_view")
+
