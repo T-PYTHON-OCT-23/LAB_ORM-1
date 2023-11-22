942810f371c977ab457017d4293c52f0385daf53
AseelAlshammrani <AseelAlshammrani@gmail.com>
2023-11-22 13:07:52 +0300

    Done

--- BlogWeb/Blog/urls.py
+++ BlogWeb/Blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name = "Blog"
 urlpatterns = [
     path('read/',views.read_blog,name='read_blog'),
-    path('add/',views.add_blog,name='add_blog')
+    path('add/',views.add_blog,name='add_blog'),
+    path('detail/<blog_id>/',views.blog_detail,name='blog_detail'),
+    path('doesnotexist/',views.not_exist,name='not_exist')
 ]
--- BlogWeb/Blog/views.py
+++ BlogWeb/Blog/views.py
@@ -13,3 +13,14 @@ def add_blog(request : HttpRequest):
         new_blog.save()
         return redirect("Blog:read_blog")
     return render(request,'Blog/add_blog.html')
+
+def blog_detail(request : HttpRequest,blog_id):
+    try:
+        blog=Blog1.objects.get(id=blog_id)
+    except Exception as e :
+        return render(request,'Blog/not_exist.html')
+    return render(request,'Blog/blog_detail.html',{'blog':blog})
+
+
+def not_exist(request : HttpRequest):
+    return render(request,'Blog/not_exist.html')
