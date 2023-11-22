c211d8389531c66f04ed05f977078628e144a2ba
alya <alyamem80@gmail.com>
2023-11-22 21:12:23 +0300

    update + delete is done

--- Blog/lapblog/views.py
+++ Blog/lapblog/views.py
@@ -9,8 +9,8 @@ def add_blog_view(request: HttpRequest):
     if request.method == "POST":
         new_Web = Web(Title =request.POST["Title"], Contant =request.POST["Contant"], is_published =request.POST["is_published"], published_at=request.POST["published_at"])
         new_Web.save()
-
-        return redirect("lapblog:blog_home_view")
+        blog_id=new_Web.id
+        return redirect("lapblog:blog_detail_view",blog_id)
 
     return render(request, "lapblog/add.html")
 
@@ -21,3 +21,39 @@ def blog_home_view(request: HttpRequest):
     lapblog = Web.objects.all()
 
     return render(request, "lapblog/blog_home.html", {"lapblog" : lapblog})
+
+def blog_detail_view(request:HttpRequest, blog_id):
+    try:
+        blog = Web.objects.get(id = blog_id)
+    except Exception as e:
+        return render(request, "lapblog/NotExist.html")
+   
+
+    return render(request, "lapblog/blog_detail.html", {"post" : blog})
+
+def NotExist_view(request:HttpRequest):
+
+    return render (request , "NotExist.html" )
+def update_blog_view(request:HttpRequest , blog_id):
+    blog = Web.objects.get(id = blog_id)
+    return render(request)
+
+def update_blog_view(request: HttpRequest , blog_id ):
+    blog = Web.objects.get(id = blog_id)
+    if request .method == "POST":
+        Web.Title = request.POST["Title"]
+        Web.Contant = request.POST["Contant"]
+        Web.is_published = request. POST["is_published"]
+        Web.published_at = request . POST["published_at"]
+        Web.save()
+        return redirect ('lapblog:blog_detail_view' , blog_id=Web.id)
+    
+    return render(request, "lapblog/update.html", {"Blogs" : blog})
+
+
+def delete_blog_view(request: HttpRequest, blog_id):
+
+    blog = Web.objects.get(id = blog_id)
+    Web.delete()
+
+    return redirect("lapblog:blog_home_view")
