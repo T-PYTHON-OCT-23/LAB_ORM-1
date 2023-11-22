4f4fdd78449a007e8624c7c07eae09662e30e632
alya <alyamem80@gmail.com>
2023-11-22 10:23:35 +0300

    done

--- Blog/lapblog/views.py
+++ Blog/lapblog/views.py
@@ -7,7 +7,7 @@ def add_blog_view(request: HttpRequest):
     #Creating a new entry into the database for a movie
 
     if request.method == "POST":
-        new_Web = Web(Title =request.POST["Title"], Contant =request.POST["Contant"], is_published =request.POST[" is_published"], published_at=request.POST["published_at"])
+        new_Web = Web(Title =request.POST["Title"], Contant =request.POST["Contant"], is_published =request.POST["is_published"], published_at=request.POST["published_at"])
         new_Web.save()
 
         return redirect("lapblog:blog_home_view")
