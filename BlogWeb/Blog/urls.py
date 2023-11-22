4cd298d30f74de932b76985befc5b7246c793ebf
AseelAlshammrani <AseelAlshammrani@gmail.com>
2023-11-22 21:16:45 +0300

    done

--- BlogWeb/Blog/urls.py
+++ BlogWeb/Blog/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('read/',views.read_blog,name='read_blog'),
     path('add/',views.add_blog,name='add_blog'),
     path('detail/<blog_id>/',views.blog_detail,name='blog_detail'),
-    path('doesnotexist/',views.not_exist,name='not_exist')
+    path('doesnotexist/',views.not_exist,name='not_exist'),
+    path('update/<blog_id>/',views.update_blog,name='update_blog'),
+    path('delete/<blog_id>/',views.delete_blog,name='delete_blog')
+
 ]
--- BlogWeb/Blog/views.py
+++ BlogWeb/Blog/views.py
@@ -24,3 +24,20 @@ def blog_detail(request : HttpRequest,blog_id):
 
 def not_exist(request : HttpRequest):
     return render(request,'Blog/not_exist.html')
+
+def update_blog(request : HttpRequest,blog_id):
+    blog=Blog1.objects.get(id=blog_id)
+    if request.method=="POST":
+        blog.title=request.POST['title']
+        blog.content=request.POST['content']
+        blog.is_published=request.POST['is_published']
+        blog.published_at=request.POST['published_at']
+        blog.save()
+        return redirect('Blog:blog_detail',blog_id=blog.id)
+    return render(request,'Blog/update_blog.html',{"blog" : blog})
+
+def delete_blog(request: HttpRequest, blog_id):
+    blog = Blog1.objects.get(id=blog_id)
+    blog.delete()
+
+    return redirect("Blog:read_blog")
