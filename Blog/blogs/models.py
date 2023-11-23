c4de5c020da58e6fc79215534049b45f33be4844
naif <iimnaif@gmail.com>
2023-11-23 16:41:38 +0300

    done

--- Blog/blogs/models.py
+++ Blog/blogs/models.py
@@ -3,9 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
+    categories = models.TextChoices("Categories", ["Dialogues", "Articles", "Stories", "Fantasy"])
+    
     title = models.CharField(max_length=2048)
     content = models.TextField()
     is_published = models.BooleanField(default=False)
     published_at = models.DateField()
+    poster = models.ImageField(upload_to="images/", default="images/default.jpg")
+
     
     
--- Blog/blogs/views.py
+++ Blog/blogs/views.py
@@ -6,12 +6,12 @@ from .models import Blog
 
 def add_blogs_view(request:HttpRequest):
     if request.method == "POST":
-        new_blogs = Blog(title=request.POST["title"], content=request.POST["content"], is_published = request.POST["is_published"], published_at = request.POST["published_at"])
+        new_blogs = Blog(title=request.POST["title"], content=request.POST["content"], is_published = request.POST["is_published"], published_at = request.POST["published_at"], poster=request.FILES["poster"])
         new_blogs.save()
         return redirect("blogs:show_blogs_view")
 
     
-    return render(request,"blogs/add.html")
+    return render(request,"blogs/add.html", {"categories" : Blog.categories})
 
 def show_blogs_view(request:HttpRequest):
     
@@ -42,7 +42,7 @@ def blog_update_view(request:HttpRequest, blog_id):
         return redirect('blogs:blog_detail_view',blog_id=blog_id)
 
     
-    return render(request,"blogs/update.html", {"blog" : blog})
+    return render(request,"blogs/update.html", {"blog" : blog, "categories"  : Blog.categories})
 
 def blog_delete_view(request:HttpRequest, blog_id):
         blog = Blog.objects.get(id=blog_id)
