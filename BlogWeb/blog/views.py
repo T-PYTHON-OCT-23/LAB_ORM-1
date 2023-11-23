5912c25e518040d52784817f7c656b3ded795164
Reef <reef.zayed2@gmail.com>
2023-11-23 15:04:10 +0300

    Done

--- BlogWeb/blog/views.py
+++ BlogWeb/blog/views.py
@@ -8,11 +8,13 @@ from django.utils import timezone
 
 def add_blog_views(request: HttpRequest):
     if request.method == "POST":
-        new_blog = Blog(title=request.POST["title"], content=request.POST["content"],  is_published=request.POST["is_published"], published_at=timezone.now())
+        new_blog = Blog(title=request.POST["title"],content=request.POST["content"],is_published=request.POST["is_published"],category=request.POST["category"],published_at=timezone.now())
+        if "image" in request.FILES:
+            new_blog.image=request.FILES["image"]
         new_blog.save()
         return redirect("blog:home_blog_views")
 
-    return render(request, "blog/add_blog.html")
+    return render(request, "blog/add_blog.html",{"categories":Blog.categories})
 
 
 
@@ -20,7 +22,7 @@ def home_blog_views(request: HttpRequest):
      
     blog = Blog.objects.all()
 
-    return render(request, "blog/home_blog.html", {"blog":blog})
+    return render(request, "blog/home_blog.html", {"blogs":blog})
 
 def details_blog_views(request: HttpRequest,blog_id):
         try:
@@ -38,13 +40,13 @@ def updated_blog(request:HttpRequest, blog_id):
         blogs.title = request.POST["title"]
         blogs.content = request.POST["content"]
         blogs.is_published = request.POST["is_published"]
-        
         blogs.published_at=timezone.now()
+        blogs.category=request.POST["category"]
         blogs.save()
 
         return redirect('blog:details_blog_views', blog_id=blogs.id)
      
-    return render(request,"blog/updated_blog.html",{"blogs":blogs})
+    return render(request,"blog/updated_blog.html",{"blogs":blogs ,"categories":Blog.categories})
 
 def delete_blog_views (request:HttpRequest, blog_id):
     blogs= Blog.objects.get(id=blog_id)
