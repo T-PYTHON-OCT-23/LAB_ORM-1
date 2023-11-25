f8ec739baccadca708be2689c1892532b184a347
khamis_dossary <khamis.dossary@gmail.com>
2023-11-25 22:33:53 +0300

    added category and image

--- blog/main/forms.py
+++ blog/main/forms.py
@@ -4,4 +4,8 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'is_published']
+        fields = ['title', 'content', 'is_published', 'category', 'image']
+        widgets = {
+            'category': forms.Select(attrs={'class': 'custom-select'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'custom-file-input'}),
+        }
--- blog/main/views.py
+++ blog/main/views.py
@@ -5,8 +5,7 @@ from .forms import PostForm
 from django.urls import reverse
 
 def post_list(request):
-    try :
-        # posts = Post.objects.filter(is_published=True, published_at__lte=timezone.now()).order_by('-published_at')
+    try:
         posts = Post.objects.all()
         return render(request, 'blog/post_list.html', {'posts': posts})
     except Exception as e:
@@ -22,30 +21,25 @@ def post_detail(request, pk):
 def post_create(request):
     try:
         if request.method == 'POST':
-            form = PostForm(request.POST)
+            form = PostForm(request.POST, request.FILES)
             if form.is_valid():
                 post = form.save(commit=False)
                 if post.is_published:
                     post.published_at = timezone.now()
-                    post.save()
-
-                    return redirect(reverse('main:post_detail', kwargs={'pk': post.pk}))
-                # else:
-                #     post.save()
-                #     return redirect('main:post_list')
+                post.save()
+                return redirect(reverse('main:post_detail', kwargs={'pk': post.pk}))
         else:
             form = PostForm()
         return render(request, 'blog/post_form.html', {'form': form})
     except Exception as e:
         return render(request, 'blog/error.html')
-    
+
 def post_update(request, pk):
     try:
-        
         post = get_object_or_404(Post, pk=pk)
 
         if request.method == 'POST':
-            form = PostForm(request.POST, instance=post)
+            form = PostForm(request.POST, request.FILES, instance=post)
             action = request.POST.get('action')
 
             if form.is_valid():
@@ -65,7 +59,6 @@ def post_update(request, pk):
         else:
             form = PostForm(instance=post)
 
-        return render(request, 'blog/post_update.html', {'form': form,'post': post})
-    
+        return render(request, 'blog/post_update.html', {'form': form, 'post': post})
     except Exception as e:
         return render(request, 'blog/error.html')
